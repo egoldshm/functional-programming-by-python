c020cb6720782a1b6b3facff4d75636c340b39ca
איתן גולדשמידט <eitntt@gmail.com>
2019-04-25 18:06:43 +0300

    Update py7.py

--- 3/py7.py
+++ 3/py7.py
@@ -1,13 +1,21 @@
 import eratosthenes
 def twinp(n):
-    return dict(zip(filter(lambda num: num + 2 in eratosthenes.napa(n) ,eratosthenes.napa(n)),
-filter(lambda num: num - 2 in eratosthenes.napa(n) ,eratosthenes.napa(n))))
+    def _twinp(arr):
+        if(len(arr)<2):
+            return {}
+        if arr[0] + 2 == arr[1]:
+            return {**{arr[0]: arr[1]}, **_twinp(arr[1:])}
+        return _twinp(arr[1:])
+    return _twinp(eratosthenes.napa(n))
 
 def printDictLineByLine(dic):
-    print('\n'.join(map(lambda num1,num2:str(num1) + ", " + str(num2),list(dic.keys()),list(dic.values()))))
-
+    if(len(dic.items()) < 1):
+        return
+    print(str(list(dic.items())[0][0]) + ", " + str(list(dic.items())[0][1]))
+    printDictLineByLine(dict(list(dic.items())[1:]))
+    
 def inputNumAndPrintTwinPrime():
-   printDictLineByLine(twinp(int(input("Enter a number for twin prime number:\n>"))))
+    printDictLineByLine(twinp(int(input("Enter a number for twin prime number:\n>"))))
 
 if __name__ == "__main__":
     inputNumAndPrintTwinPrime()
